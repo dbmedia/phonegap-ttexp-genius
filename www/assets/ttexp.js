dc555ca433d4d19c0d714f4708f3f45b31b3603b
AndreaMauro@DBMedia <andrea.mauro@cheapnet.it>
2017-02-16 16:13:17 +0100

    no message

--- www/assets/ttexp.js
+++ www/assets/ttexp.js
@@ -11139,7 +11139,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("ttexp/app")["default"].create({"serverApiUrl":"http://ttexp-server.localhost/api","LOG_ACTIVE_GENERATION":true,"LOG_VIEW_LOOKUPS":true,"name":"ttexp","version":"1.2.0+7ba8ce23"});
+  require("ttexp/app")["default"].create({"serverApiUrl":"http://demo.ttexp.net/api","LOG_ACTIVE_GENERATION":false,"LOG_VIEW_LOOKUPS":false,"name":"ttexp","version":"1.2.0+7ba8ce23"});
 }
 
 /* jshint ignore:end */
